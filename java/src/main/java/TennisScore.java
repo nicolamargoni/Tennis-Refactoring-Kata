5730d1a891341c57a0e2676f573216c811ef8da2
nicolamargoni <nicola.margoni@xpeppers.com>
2017-10-13 16:16:08 +0200

    refactor: Extraction Values, added static final strings with value of score to TennisScore.class

--- java/src/main/java/TennisScore.java
+++ java/src/main/java/TennisScore.java
@@ -1,4 +1,21 @@
 public class TennisScore {
+    static final String SCORE_LOVE_ALL = "Love-All";
+    static final String SCORE_FIFTEEN_ALL = "Fifteen-All";
+    static final String SCORE_THIRTY_ALL = "Thirty-All";
+    static final String SCORE_DEUCE = "Deuce";
+    static final String SCORE_ADVANTAGE_PLAYER_ONE = "Advantage player1";
+    static final String SCORE_ADVANTAGE_PLAYER_TWO = "Advantage player2";
+    static final String SCORE_WIN_PLAYER_ONE = "Win for player1";
+    static final String SCORE_WIN_PLAYER_TWO = "Win for player2";
+
+    static final String SCORE_LOVE = "Love";
+    static final String SCORE_FIFTEEN = "Fifteen";
+    static final String SCORE_THIRTY = "Thirty";
+    static final String SCORE_FORTY = "Forty";
+    static final String SCORE_SEPARATOR = "-";
+    
+    static final String STRING_EMPTY = "";
+
     private TennisPlayer player1;
     private TennisPlayer player2;
 
@@ -38,16 +55,16 @@ public class TennisScore {
         String result;
         switch (player1.getScore()) {
             case 0:
-                result = "Love-All";
+                result = SCORE_LOVE_ALL;
                 break;
             case 1:
-                result = "Fifteen-All";
+                result = SCORE_FIFTEEN_ALL;
                 break;
             case 2:
-                result = "Thirty-All";
+                result = SCORE_THIRTY_ALL;
                 break;
             default:
-                result = "Deuce";
+                result = SCORE_DEUCE;
                 break;
         }
         return result;
@@ -56,34 +73,34 @@ public class TennisScore {
     private String getScoreWhenAPlayerIsAdvantageOrWin() {
         String result;
         int minusResult = player1.getScore() - player2.getScore();
-        if (minusResult == 1) result = "Advantage player1";
-        else if (minusResult == -1) result = "Advantage player2";
-        else if (minusResult >= 2) result = "Win for player1";
-        else result = "Win for player2";
+        if (minusResult == 1) result = SCORE_ADVANTAGE_PLAYER_ONE;
+        else if (minusResult == -1) result = SCORE_ADVANTAGE_PLAYER_TWO;
+        else if (minusResult >= 2) result = SCORE_WIN_PLAYER_ONE;
+        else result = SCORE_WIN_PLAYER_TWO;
         return result;
     }
 
     private String getScoreWhenTheScoresAreDifferent() {
-        String result = "";
+        String result = STRING_EMPTY;
         int tempScore = 0;
         for (int i = 1; i < 3; i++) {
             if (i == 1) tempScore = player1.getScore();
             else {
-                result += "-";
+                result += SCORE_SEPARATOR;
                 tempScore = player2.getScore();
             }
             switch (tempScore) {
                 case 0:
-                    result += "Love";
+                    result += SCORE_LOVE;
                     break;
                 case 1:
-                    result += "Fifteen";
+                    result += SCORE_FIFTEEN;
                     break;
                 case 2:
-                    result += "Thirty";
+                    result += SCORE_THIRTY;
                     break;
                 case 3:
-                    result += "Forty";
+                    result += SCORE_FORTY;
                     break;
             }
         }

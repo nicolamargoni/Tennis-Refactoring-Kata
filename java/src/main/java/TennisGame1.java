42b9589295e316403ef2240faa7124e675fd1276
nicolamargoni <nicola.margoni@xpeppers.com>
2017-10-13 14:21:39 +0200

    refactor: Extract Method, if a player win the set or is in the lead I call the new method extracted for generate the score result

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -24,11 +24,7 @@ public class TennisGame1 implements TennisGame {
         if (m_score1 == m_score2) {
             score = getScoreWhenIsEqual();
         } else if (m_score1 >= 4 || m_score2 >= 4) {
-            int minusResult = m_score1 - m_score2;
-            if (minusResult == 1) score = "Advantage player1";
-            else if (minusResult == -1) score = "Advantage player2";
-            else if (minusResult >= 2) score = "Win for player1";
-            else score = "Win for player2";
+            score = getScoreWhenAPlayerIsAdvantageOrWin();
         } else {
             for (int i = 1; i < 3; i++) {
                 if (i == 1) tempScore = m_score1;
@@ -71,6 +67,16 @@ public class TennisGame1 implements TennisGame {
                 result = "Deuce";
                 break;
         }
+        return result;
+    }
+
+    private String getScoreWhenAPlayerIsAdvantageOrWin() {
+        String result;
+        int minusResult = m_score1 - m_score2;
+        if (minusResult == 1) result = "Advantage player1";
+        else if (minusResult == -1) result = "Advantage player2";
+        else if (minusResult >= 2) result = "Win for player1";
+        else result = "Win for player2";
         return result;
     }
 }
